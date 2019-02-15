412217763f28485f0bcd46806526852b8e4bcf0d
Z001DSN <Z001DSN@HTFXY52.hq.target.com>
2019-02-14 21:58:42 -0600

    Added code for FB Login page and added selenium drivers

--- Sample_Github/src/github_sample/Test2.java
+++ Sample_Github/src/github_sample/Test2.java
@@ -1,11 +1,27 @@
 package github_sample;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test2 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("How are you");
-		// TODO Auto-generated method stub
+		
+		WebDriver driver;
+
+		// Launch chrome Browser
+		// System.setProperty(key, value);
+
+		System.setProperty("webdriver.chrome.driver", ".\\BrowserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// Enter URL
+		// driver.get(arg0);
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
 
 	}
 
