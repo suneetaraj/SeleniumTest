9f901c3acdcbc4606b4ebaf9eae15c4925d7ecde
Z001DSN <Z001DSN@HTFXY52.hq.target.com>
2019-02-14 21:13:01 -0600

    Added new class Test2

---
+++ Sample_Github/src/github_sample/Test2.java
@@ -0,0 +1,12 @@
+package github_sample;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("How are you");
+		// TODO Auto-generated method stub
+
+	}
+
+}
